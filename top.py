79fa24c121886a28908b425ed8fb0f880cff9b6b
MathieuKoz <mathieu.koz@orange.fr>
2024-11-18 17:15:39 +0100

    update initialisation

--- top.py
+++ top.py
@@ -16,8 +16,10 @@ m=4 #nombre de véhicules
 C_lim=L #limite de coût d'un arc (modifiable)
 P_lim=100 #limite de profit sur un client
 
-C=[] #matrice des coûts
-P=[] #matrice des profits
+C=[] #Arcs avec coût
+P=[] #Liste des profits
+R=[] #Liste des routes
+X=np.zeros(n) #Liste des clients visités
 
 d=0
 a=n+1
@@ -25,15 +27,18 @@ a=n+1
 G=nx.Graph()
 for i in range(n+2): #0 = d et n+1=a
     V.append(i)
-    P.append(random.randint(0,P_lim))
+
     for j in range(n+2):
         if i !=j:
             E.append((i,j))
             C.append((i,j, random.randint(1,L)))
 
+for i in range(n):
+    P.append(random.randint(0,P_lim))
+
 G.add_weighted_edges_from(C)
 
-####################
+###########################
 
 print(C)
 nx.draw(G, with_labels=True)
