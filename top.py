bc5b84d8368a24bc1034588630f9040f36bd845b
yannickLonjarret <86044285+yannickLonjarret@users.noreply.github.com>
2024-11-19 12:03:09 +0100

    first push because I forgor

--- top.py
+++ top.py
@@ -2,7 +2,7 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
-
+import copy
 
 
 ######Initialisation
@@ -11,9 +11,9 @@ V=[]
 E=[]
 
 n=10 #nombre de client
-L=100 #limite de temps
+L=1000 #limite de temps
 m=4 #nombre de véhicules
-C_lim=L #limite de coût d'un arc (modifiable)
+C_lim=100 #limite de coût d'un arc (modifiable)
 P_lim=100 #limite de profit sur un client
 
 C=[] #Arcs avec coût
@@ -38,9 +38,40 @@ for i in range(n):
 
 G.add_weighted_edges_from(C)
 
+def initial_routes_creation():
+    current_computed_route = []
+    voisins = list(G.adj[d])
+
+
+    for i in range(len(voisins)-1):
+        current_computed_route.append(d)
+        current_computed_route.append(voisins[i])
+        current_computed_route.append(a)
+        deep_copy = copy.deepcopy(current_computed_route)
+        R.append(deep_copy)
+        current_computed_route.clear()
+
+def compute_savings(all_nodes):
+    for i in range(1,len(all_nodes)-1):
+        for j in range(1,len(all_nodes)-1):
+            if i != j:
+                savingIJ = G.edges[i, n+1]['weight'] + G.edges[0, j]['weight'] - G.edges[i, j]['weight']
+                if savingIJ >= 0: 
+                    savings.append([i, j, savingIJ])
+
+def
+###########################
+initial_routes_creation()
+voisins = list(G.adj[d])
+savings = []
+compute_savings(voisins)
 ###########################
 
-print(C)
-nx.draw(G, with_labels=True)
-plt.show()
+
+
+savings.sort(key=lambda x: x[2], reverse=True)
+print(savings)
+print(G.edges.data())
+#nx.draw(G, with_labels=True)
+#plt.show()
 
